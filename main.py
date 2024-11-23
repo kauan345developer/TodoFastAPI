f3ec49084bf437e12e5cfc98f063d42bc0995b0d
kauan345developer <kauanolival@gmail.com>
2024-11-23 20:18:00 -0300

    adicionado delete e patch

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
-from schemas import TodoCreate
+from schemas import TodoCreate,TodoEdit
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 
 from models import Todo
@@ -40,4 +40,39 @@ def todo_list(session: Session = Depends(get_session)):
 def todo(todo_id:int,session: Session = Depends(get_session)):
   todo = session.scalar(select(Todo).where(Todo.id == todo_id))
   return todo
+
+@app.patch("/todo/{todo_id}")
+def todo_edit(todo_id:int,todo:TodoEdit,session: Session = Depends(get_session)):
+    
+    db_todo = session.scalar(
+        select(Todo).where(Todo.id == todo_id)
+    )
+    
+    # if not todo:
+    #     raise HTTPException(
+    #         status_code=HTTPStatus.NOT_FOUND, detail='Task not found.'
+    #     )
+    # print(todo.model_dump())
+    # print(**todo.model_dump())
+    
+    for key, value in todo.model_dump(exclude_unset=True).items():
+        setattr(db_todo, key, value)
+
+    # query = update(Todo).where(Todo.id == todo_id).values(Todo(title=todo.title, description=todo.description))
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+    return db_todo
+
+
+@app.delete("/todo/{todo_id}")
+def todo_delete(todo_id:int,session: Session = Depends(get_session)):
+    db_todo = session.scalar(
+        select(Todo).where(Todo.id == todo_id)
+    )
+    
+    session.delete(db_todo)
+    session.commit()
+    
+    return {'message': 'Task has been deleted successfully.'}
 
--- schemas.py
+++ schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class TodoCreate(BaseModel):
     title: str
     description: str
+
+class TodoEdit(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    
